8ade0a257378f5dddd7c71302586e4c1e23fe443
RehmatGul0 <43272096+RehmatGul0@users.noreply.github.com>
2018-10-12 23:54:02 -0700

    startWebFromServer

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+router.use(express.static(path.join(__dirname,'../','public')));
+/* GET home page.*/
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname,'../','Views/index.html'));
 });
-
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var bodyParser = require('body-parser');
+router.use(bodyParser.json());
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/appointment', function(req, res, next) {
+  console.log(req.body);
+  res.send();
 });
 
 module.exports = router;
