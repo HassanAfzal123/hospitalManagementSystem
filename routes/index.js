f2ad8172c98132aec8244496fc27bf02a546aee5
RehmatGul0 <43272096+RehmatGul0@users.noreply.github.com>
2018-10-17 00:24:16 -0700

    problemSolved
    merge conflict removed in cafeteria

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const path = require('path');
-<<<<<<< HEAD
-const reqapp = require('../database');
-=======
 const db = require('../database');
->>>>>>> 6a1de9c95e0f6225ac05433c829236ff32648062
+
 const router = express.Router();
 router.use(express.static(path.join(__dirname,'../','public')));
 /* GET home page.*/
@@ -15,20 +12,14 @@ router.post('/recipt', function(req, res, next) {
     var reportid = req.body.reportid;
     var queryString = 'SELECT * FROM reports WHERE reportId=?';
 
-<<<<<<< HEAD
-    reqapp.connection.query(queryString, reportid, function(err, result) {
+    
+    db.connection.query(queryString, reportid, function(err, result) {
+        console.log(result);
         try{
             res.sendFile(path.join(__dirname, '../', result[0].resultAddress + ".pdf"));
         }
         catch(err){
             res.send("WRONG ID");
-=======
-    db.connection.query(queryString, reportid, function(err, rows, fields) {
-        if (err) throw err;
-
-        for(i in rows){
-          res.send("Name: "+rows[i].name+ "<br>TestName: "+rows[i].testName+"<br>Test Result: "+rows[i].result);
->>>>>>> 6a1de9c95e0f6225ac05433c829236ff32648062
         }
 
     });
