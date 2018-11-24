8638fae1776c694798a3bdda227b4af7457513c1
Hassan Afzal <hassan_afzal6@yahoo.com>
2018-11-25 04:24:15 +0500

    User Info Updating system added.

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,7 @@ router.use(bodyParser.json());
 router.use(express.static(path.join(__dirname,'../','public')));
 /* GET users listing. */
 router.get('/home', function(req, res, next) {
+
     let queryString = "SELECT P.first_name,P.last_name,P.gender,P.address,P.cell_no,U.email from PATIENT P,USER_INFO U where U.info_id = P.USER_INFO_info_id AND patient_id = ?";
   if(req.session.user) {
       db.connection.query(queryString,[req.session.user],function (err,result,fields) {
@@ -14,7 +15,6 @@ router.get('/home', function(req, res, next) {
               throw err;
           }
           else {
-              console.log(result[0]);
               res.render(path.join(__dirname,'../','Views/layouts/dashboard.hbs'),{user: result[0]});
           }
       });
@@ -24,6 +24,7 @@ router.get('/home', function(req, res, next) {
   {
     res.send("You are not logged in!");
   }
+
 });
 router.get('/reports', function (req,res,next) {
     if(req.session.user) {
@@ -36,10 +37,28 @@ router.get('/reports', function (req,res,next) {
 
 });
 router.get('/logout', function(req, res, next) {
+    if(req.session.user) {
         req.session.destroy();
-    res.locals.info = null;
-    res.locals.success=null;
-        res.render(path.join(__dirname,'../','Views/login.ejs'));
+        res.locals.info = null;
+        res.locals.success = null;
+        res.render(path.join(__dirname, '../', 'Views/login.ejs'));
+    }
+});
+router.post('/update', function (req,res,next) {
+    if(req.session.user){
+        let queryString = "UPDATE PATIENT SET ? where PATIENT.patient_id = ?";
+        let newUser_info = {first_name: req.body.FnewName, last_name: req.body.LnewName, address: req.body.newAddress};
+        console.log(newUser_info);
+        db.connection.query(queryString,[newUser_info,req.session.user],function(err2,rows,fields) {
+           if(err2){
+               throw err2;
+           }
+           else {
 
+               res.redirect('/user/home');
+
+           }
+        });
+    }
 });
 module.exports = router;
