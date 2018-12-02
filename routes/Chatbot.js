db9995625c4748d4efa1c70ea89e7eaa444c3a0a
Hassan Afzal <hassan_afzal6@yahoo.com>
2018-12-03 00:14:57 +0500

    CHATBOT

--- routes/Chatbot.js
+++ routes/Chatbot.js
@@ -4,7 +4,7 @@ const db = require('../database');
 const router = express.Router();
 const TextRazor = require('textrazor');
 const textRazor = new TextRazor('8e3f98862a3fd4aa7b9b3b391e1e14dc56771b60cd36e1c92465a850');
-const content = 'Get me a physician for cholera';
+const content = 'Get me a physician for cancer,Hepatitis and malaria';
 const options = { extractors: 'words,entailments,entities'};//,categoies,relations,properties' };
 // router.get('/', function (req, res) {
 //     res.sendFile(path.join(__dirname,'../','views/chatbot.html'));
@@ -26,16 +26,33 @@ router.get('/',(req,res)=>{
         });
         //If there is a question about doctor..
         if(check==0){
-            console.log("Invalid querry..");
+            console.log("Invalid query..");
         }
         else{
-            result.response.entities.forEach(element=>{
+            result.response.entities.forEach(async element=>{
                 if(element.type=='Disease'){
                     console.log(element.entityId);
-                    disease.push(element.entityId);
+                    await  disease.push(element.entityId);
                 }
-            })
+            });
             check=0;
+            var j;
+            console.log(disease);
+            let get_Doctor = "SELECT DOCTOR.name,DISEASE.name as Disease FROM DOCTOR,DOCTOR_DISEASE,DISEASE where DOCTOR.doctor_id = DOCTOR_DISEASE.DOCTOR_doctor_id AND DISEASE.disease_id = DOCTOR_DISEASE.DISEASE_disease_id AND DISEASE.name = ?"
+            console.log("Here is the list of the doctors for your disease: ");
+            for(j=0;j < disease.length;j++)
+            {
+                db.connection.query(get_Doctor, [disease[j]], function (err, doctor) {
+                    if (err) throw err;
+                    if(doctor.length < 1) {
+                        console.log("Sorry,we have no doctor available for"); //SHOW DISEASE NAME HERE
+                    }
+                    else {
+                         console.log(doctor);
+                    }
+
+                });
+            }
         }
         res.send(result);
         
